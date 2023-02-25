dcd5234ba403d85be64d985d861c102ea8b59c3d
wnstjq7979 <wnstjq7979@gmail.com>
2023-02-25 19:55:14 +0900

    정률 할인 정책 적용을 위해 OrderServiceImpl 수정

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package hello.core.discount;
 
+// DiscountPolicy의 정률 금액 할인 구현체.
+
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
 
+    // VIP 고정 할인 10%
     private int discountPercent = 10;
 
     @Override
