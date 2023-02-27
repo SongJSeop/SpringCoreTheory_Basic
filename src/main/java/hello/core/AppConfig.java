f478c6ee8156b33f1aa9dbf5870943c462f50273
wnstjq7979 <wnstjq7979@gmail.com>
2023-02-27 22:28:52 +0900

    정액 할인에서 정률 할인으로 교체하기 위해 AppConfig 코드 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,7 +3,7 @@ package hello.core;
 // DIP, OCP를 지키기 위해(추상, 즉 인터페이스에만 서로 의존하게 하기 위해) 구현체를 대신 주입해주는 Config 파일.
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -29,8 +29,8 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    // 할인 정책은 FixDiscountPolicy를 사용.
+    // 할인 정책은 RateDiscountPolicy를 사용.
     private static DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
