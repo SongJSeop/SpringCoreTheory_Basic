65a5a799b62f35de5a5909066b5ae2df1c173644
wnstjq7979 <wnstjq7979@gmail.com>
2023-03-18 15:26:03 +0900

    싱글톤 패턴이 필요한 이유를 보기 위한 테스트코드 작성

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 서로 다르다. 클라이언트들이 동시에 요청을 하면 그 때마다 새로운 객체를 생성한다는 뜻. 효율적이지 않음(메모리 낭비).
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+        // 그러므로 해당 객체가 그때마다 아닌 딱 하나만 생성되고 그 객체가 공유되어 모두 처리하면 효율적임. -> 싱글톤 패턴
+    }
+}
