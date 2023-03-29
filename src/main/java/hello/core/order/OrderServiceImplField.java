0f5149b6c03b003b2026c80d40a31d8f03dcbfee
wnstjq7979 <wnstjq7979@gmail.com>
2023-03-29 14:33:46 +0900

    필기용 OrderService 구현체들 주석처리

--- src/main/java/hello/core/order/OrderServiceImplField.java
+++ src/main/java/hello/core/order/OrderServiceImplField.java
@@ -7,25 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // 수정자주입 필기용 클래스
-@Component
-public class OrderServiceImplField implements OrderService {
+//@Component
+//public class OrderServiceImplField implements OrderService {
 
     /* 의존관계 주입 3. 필드 주입
         의존관계를 필드에 바로 넣어줌. 코드는 간결하지만 외부에서 변경이 불가능해 테스트가 힘들다. 그래서 추천되는 방식은 아님.
         애플리케이션과 관계 없는 테스트코드나 설정 목적으로하는 @Configuration 같은 곳에서만 특별한 용도로 사용하도록 하자.*/
-    @Autowired private MemberRepository memberRepository;
-    @Autowired private DiscountPolicy discountPolicy;
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
 
-    @Override
-    public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
-
-        return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
-
-    // 테스트용 메소드
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
-}
+//    @Override
+//    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+//        Member member = memberRepository.findById(memberId);
+//        int discountPrice = discountPolicy.discount(member, itemPrice);
+//
+//        return new Order(memberId, itemName, itemPrice, discountPrice);
+//    }
+//
+//    // 테스트용 메소드
+//    public MemberRepository getMemberRepository() {
+//        return memberRepository;
+//    }
+//}
--- src/main/java/hello/core/order/OrderServiceImplInit.java
+++ src/main/java/hello/core/order/OrderServiceImplInit.java
@@ -1,36 +1,36 @@
-package hello.core.order;
-
-import hello.core.discount.DiscountPolicy;
-import hello.core.member.Member;
-import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-// 일반 메서드 주입 필기용 클래스
-@Component
-public class OrderServiceImplInit implements OrderService {
-
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    /* 의존관계 주입 4. 일반 메서드 주입
-        일반 메서드를 통해서 주입. 한번에 여러 필드를 주입 받을 수 있다. 일반적으로 잘 사용하진 않음.*/
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Override
-    public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
-
-        return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
-
-    // 테스트용 메소드
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
-}
+//package hello.core.order;
+//
+//import hello.core.discount.DiscountPolicy;
+//import hello.core.member.Member;
+//import hello.core.member.MemberRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//// 일반 메서드 주입 필기용 클래스
+//@Component
+//public class OrderServiceImplInit implements OrderService {
+//
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    /* 의존관계 주입 4. 일반 메서드 주입
+//        일반 메서드를 통해서 주입. 한번에 여러 필드를 주입 받을 수 있다. 일반적으로 잘 사용하진 않음.*/
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+//
+//    @Override
+//    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+//        Member member = memberRepository.findById(memberId);
+//        int discountPrice = discountPolicy.discount(member, itemPrice);
+//
+//        return new Order(memberId, itemName, itemPrice, discountPrice);
+//    }
+//
+//    // 테스트용 메소드
+//    public MemberRepository getMemberRepository() {
+//        return memberRepository;
+//    }
+//}
