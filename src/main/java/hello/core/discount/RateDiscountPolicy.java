5a98e20874a13b0c4d367c26cdb251cf99fcb5a3
wnstjq7979 <wnstjq7979@gmail.com>
2023-04-02 21:43:11 +0900

    조회 대상 빈이 2개일때 해결법 3: @Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -6,10 +6,14 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy")  // 조회 대상 빈이 2개일때 해결법 2: @Qualifier
+//@Qualifier("mainDiscountPolicy")  // 조회 대상 빈이 2개일때 해결법 2: @Qualifier
+@Primary  // 조회 대상 빈이 2개일때 해결법 3: @Primary를 넣으면 우선 순위로 먼저 주입이 됨.
+// 예를 들어 메인 DB와 보조 DB가 있으면 메인 DB에 @Primary를 넣어 사용하고, 보조 DB를 사용할때 만 1번 or 2번 방법을 사용하면 좋음.
+// Qulifier와 Primary가 동시에 작동하면 수동으로 작성해준 Qulifier가 우선권이 높다.
 public class RateDiscountPolicy implements DiscountPolicy {
 
     // VIP 고정 할인 10%
